35bb822c5b95db26ef23402bb48af7f2a53f2abc
Jia Xu <xujia118@hotmail.com>
2024-11-02 08:29:48 -0700

    Changed mistakes

--- test.py
+++ test.py
@@ -2,5 +2,4 @@ import os
 
 secret_key = os.getenv("SECRET")
 
-print(f"Secret key is {secret_key}") if secret_key else print("No key 
-fetched")
+print(f"Secret key is {secret_key}") if secret_key else print("No key fetched")
